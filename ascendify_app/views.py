5c3a0970f49d9c76288151a45bf0761290ae443c
dawid31 <dawidkedzierski04@gmail.com>
2024-09-10 01:05:40 +0200

    Added frontend for landpage, login & register

--- ascendify_app/views.py
+++ ascendify_app/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
-from django.http import HttpResponse
-
+    
 def index(request):
     return render(request, 'ascendify_app/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'ascendify_app/register.html', {'form': form})
+
+
+def login(request):
+    return render(request, 'ascendify_app/login.html')
