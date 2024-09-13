3b3e098999bda03866b820c05d4599636496ee9e
dawid31 <dawidkedzierski04@gmail.com>
2024-09-13 17:44:38 +0200

    added auth

--- ascendify_app/urls.py
+++ ascendify_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'), 
     path('register', views.register, name='register'), 
+    path('logout', views.logoutUser, name='logout'),
     #path('/profile', views.profile, name='profile'), 
 
 ]
--- ascendify_app/views.py
+++ ascendify_app/views.py
@@ -1,23 +1,39 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout
 
-    
 def index(request):
     return render(request, 'ascendify_app/index.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Login failed. Check fields and try again.')
+
+    return render(request, 'ascendify_app/login.html')
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             messages.success(request, 'Account created successfully!')
-            return redirect('login')
+            auth_login(request, user)  # Automatically log in the user after registration
+            return redirect('index')
     else:
         form = UserCreationForm()
 
     return render(request, 'ascendify_app/register.html', {'form': form})
 
-
-def login(request):
-    return render(request, 'ascendify_app/login.html')
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
